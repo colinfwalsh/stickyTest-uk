8667aa13919f327dfbc732aedbcfdb577e73321d
Colin Walsh <colin-walsh@PIPHMAC032.local>
2020-10-19 12:47:51 -0400

    Basic implementation working

--- stickyTest-uk/ViewController.swift
+++ stickyTest-uk/ViewController.swift
@@ -12,22 +12,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var headerHeight: NSLayoutConstraint!
     @IBOutlet weak var header: UIView!
+    @IBOutlet weak var headerTopConstraint: NSLayoutConstraint!
     
     private let sectionArr = ["Blah", "Blah", "Blah", "Blah",
                               "Blah", "Blah", "Blah", "Blah",
                               "Blah", "Blah", "Blah", "Blah"]
     
     private let sectionDict = [0 : ["Blah", "Blah"],
-        1 : ["Blah", "Blah"],
-        2 : ["Blah", "Blah"],
-        3 : ["Blah", "Blah"],
-        4 : ["Blah", "Blah"],
-        5 : ["Blah", "Blah"],
-        6 : ["Blah", "Blah"],
-        7 : ["Blah", "Blah"],
-        8 : ["Blah", "Blah"],
-        9 : ["Blah", "Blah"],
-       10 : ["Blah", "Blah"]]
+                               1 : ["Blah", "Blah"],
+                               2 : ["Blah", "Blah"],
+                               3 : ["Blah", "Blah"],
+                               4 : ["Blah", "Blah"],
+                               5 : ["Blah", "Blah"],
+                               6 : ["Blah", "Blah"],
+                               7 : ["Blah", "Blah"],
+                               8 : ["Blah", "Blah"],
+                               9 : ["Blah", "Blah"],
+                               10 : ["Blah", "Blah"]]
+    
+    private var updatedOffset: CGFloat = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,8 +38,21 @@ class ViewController: UIViewController {
         tableView.delegate = self
         
         tableView.showsVerticalScrollIndicator = false
+        
     }
     
+    @IBAction func touchesBegan(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: header)
+        updatedOffset = translation.y - 300
+        let updatedHeight = headerHeight.constant + updatedOffset
+        print(updatedHeight)
+        UIView.animate(withDuration: 0.2) {[weak self] in
+            guard let self = self
+            else {return}
+            
+            self.updateHeader(with: updatedHeight)
+        }
+    }
     
 }
 
@@ -77,24 +93,24 @@ extension ViewController: UITableViewDelegate {
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         
-        let updatedHeight = headerHeight.constant - scrollView.contentOffset.y
+        updatedOffset = scrollView.contentOffset.y
+        let updatedHeight = headerTopConstraint.constant - updatedOffset
         
         print(updatedHeight)
         UIView.animate(withDuration: 0.2) {[weak self] in
             guard let self = self
             else {return}
             
-            self.updateViewHierarchy(with: updatedHeight)
+            self.updateHeader(with: updatedHeight)
             scrollView.layoutIfNeeded()
-            
         }
     }
     
 }
 
 extension ViewController {
-    func updateViewHierarchy(with updatedHeight: CGFloat) {
-        self.headerHeight.constant = updatedHeight <= 55 ? 55 : updatedHeight >= 140 ? 285 : updatedHeight
+    func updateHeader(with updatedHeight: CGFloat) {
+        self.headerTopConstraint.constant = updatedHeight <= -235 ? -235 : updatedHeight >= -100 ? 0 : updatedHeight
         self.header.layoutIfNeeded()
     }
 }
